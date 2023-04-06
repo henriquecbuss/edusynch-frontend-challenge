56d57251ac20f70fc6877ed92a7c1c21a1d97138
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 00:30:58 -0300

    Implement mobile header with slideover

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import { useState } from 'react'
 import clsx from 'clsx'
 import Icons from './Icons'
 import Button from './Button'
+import SlideOver from './SlideOver'
+import Link from 'next/link'
 
 type Props = { className?: string }
 
@@ -29,9 +32,42 @@ function SignedInView({ className }: Props) {
 }
 
 function SignedOutView({ className }: Props) {
-  // TODO - Create mobile view
+  const [isSlideOverOpen, setIsSlideOverOpen] = useState(false)
+
   return (
     <>
+      <button
+        className="md:hidden ml-auto"
+        onClick={() => setIsSlideOverOpen((currState) => !currState)}
+      >
+        <Icons.Hamburger />
+      </button>
+      <SlideOver
+        isOpen={isSlideOverOpen}
+        onClose={() => setIsSlideOverOpen(false)}
+        side="right"
+        overlayClassName="top-20"
+        dialogClassName="mt-14 border-t border-secondary-300"
+      >
+        <div className="flex flex-col items-center gap-8 px-[62px]">
+          <SlideOverLink
+            href="#about-us"
+            onClick={() => setIsSlideOverOpen(false)}
+          >
+            About us
+          </SlideOverLink>
+          <SlideOverLink
+            href="#top-cryptos"
+            onClick={() => setIsSlideOverOpen(false)}
+          >
+            Top Cryptos
+          </SlideOverLink>
+          <Button variant="ghost" onClick={() => {}}>
+            Sign in
+          </Button>
+          <Button onClick={() => {}}>Sign up</Button>
+        </div>
+      </SlideOver>
       <div
         className={clsx('hidden md:flex flex-nowrap items-center', className)}
       >
@@ -43,5 +79,21 @@ function SignedOutView({ className }: Props) {
         </Button>
       </div>
     </>
+  )
+}
+
+function SlideOverLink({
+  href,
+  children,
+  onClick,
+}: {
+  href: string
+  children: React.ReactNode
+  onClick?: () => void
+}) {
+  return (
+    <Link href={href} className="text-label hover:underline" onClick={onClick}>
+      {children}
+    </Link>
   )
 }
