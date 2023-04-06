6d255388d77d8397983a4a38b24ae796ad5c0f25
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 11:57:50 -0300

    Add XMark icon

---
+++ src/components/Icons/XMark.tsx
@@ -0,0 +1,20 @@
+import clsx from 'clsx'
+
+const XMark = ({ className }: { className?: string }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      className={clsx('fill-secondary', className)}
+    >
+      <path
+        fill-rule="evenodd"
+        d="M4.104 2.913a.842.842 0 1 0-1.19 1.191L6.808 8l-3.896 3.896a.842.842 0 1 0 1.191 1.19L8 9.192l3.896 3.896a.842.842 0 0 0 1.19-1.191L9.191 8l3.895-3.896a.842.842 0 1 0-1.19-1.19L8 6.808 4.104 2.913Z"
+        clip-rule="evenodd"
+      />
+    </svg>
+  )
+}
+
+export default XMark
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,9 +1,11 @@
 import Hamburger from './Hamburger'
 import ChevronDown from './ChevronDown'
+import XMark from './XMark'
 
 const icons = {
   Hamburger,
   ChevronDown,
+  XMark,
 }
 
 export default icons
