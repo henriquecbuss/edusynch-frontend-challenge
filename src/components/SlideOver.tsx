0040393589f698dcc28015edbf4af917ca953dd2
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 11:57:42 -0300

    Use isolate class in SlideOver

--- src/components/SlideOver.tsx
+++ src/components/SlideOver.tsx
@@ -26,64 +26,66 @@ const SlideOver = ({
   dialogStyle,
 }: Props) => {
   return (
-    <Transition.Root show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10 md:hidden" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-in-out duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in-out duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div
-            className={clsx(
-              'fixed inset-0 bg-secondary-500 bg-opacity-75 transition-opacity',
-              overlayClassName
-            )}
-            style={overlayStyle}
-          />
-        </Transition.Child>
-
-        <div className="fixed inset-0 overflow-hidden">
-          <div className="absolute inset-0 overflow-hidden">
+    <div className="isolate">
+      <Transition.Root show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10 md:hidden" onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-in-out duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in-out duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
             <div
               className={clsx(
-                'pointer-events-none fixed inset-y-0 flex max-w-full',
-                { 'left-0': side === 'left', 'right-0': side === 'right' }
+                'fixed inset-0 bg-secondary-500 bg-opacity-75 transition-opacity',
+                overlayClassName
               )}
-            >
-              <Transition.Child
-                as={Fragment}
-                enter="transform transition ease-in-out duration-500 sm:duration-700"
-                enterFrom={
-                  side === 'left' ? '-translate-x-full' : 'translate-x-full'
-                }
-                enterTo="translate-x-0"
-                leave="transform transition ease-in-out duration-500 sm:duration-700"
-                leaveFrom="translate-x-0"
-                leaveTo={
-                  side === 'left' ? '-translate-x-full' : 'translate-x-full'
-                }
+              style={overlayStyle}
+            />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div
+                className={clsx(
+                  'pointer-events-none fixed inset-y-0 flex max-w-full',
+                  { 'left-0': side === 'left', 'right-0': side === 'right' }
+                )}
               >
-                <Dialog.Panel
-                  className={clsx(
-                    'pointer-events-auto relative',
-                    dialogClassName
-                  )}
-                  style={dialogStyle}
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom={
+                    side === 'left' ? '-translate-x-full' : 'translate-x-full'
+                  }
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo={
+                    side === 'left' ? '-translate-x-full' : 'translate-x-full'
+                  }
                 >
-                  <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                    {children}
-                  </div>
-                </Dialog.Panel>
-              </Transition.Child>
+                  <Dialog.Panel
+                    className={clsx(
+                      'pointer-events-auto relative',
+                      dialogClassName
+                    )}
+                    style={dialogStyle}
+                  >
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
+                      {children}
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
           </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+        </Dialog>
+      </Transition.Root>
+    </div>
   )
 }
 
