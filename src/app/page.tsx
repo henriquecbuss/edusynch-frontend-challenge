71b8db38fe96fe3e4ac4219e945a2680db60b644
Henrique Buss <henriquebusss@gmail.com>
2023-04-05 23:53:58 -0300

    Fix fontsizes label and label-small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
-    <div className="container">
-      <div className="md:w-full bg-secondary-800 h-8"></div>
+    <>
       <h1 className="text-primary text-h1 font-bold">Hello world</h1>
-    </div>
+    </>
   )
 }
