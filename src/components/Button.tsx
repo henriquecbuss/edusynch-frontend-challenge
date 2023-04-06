e81fbec9c90f589c1aad115d25fcdd63b8c739df
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 13:44:18 -0300

    Add hover styles and button type to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,21 @@
 import clsx from 'clsx'
 
 type Props = {
-  onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
   className?: string
   variant?: Variant
   children: React.ReactNode
+  type?: Type
 }
+
+type Type = 'button' | 'submit'
 
 type Variant = 'primary' | 'ghost'
 
 const defaultClassName = 'text-label'
 
 const primaryClassName =
-  'bg-primary text-white py-2 px-[26.5px] rounded-full hover:bg-primary-400 active:bg-primary-600 transition-colors'
+  'bg-primary text-white py-2 px-[26.5px] rounded-full hover:bg-primary-400 active:bg-primary-600 transition-colors focus:outline-none focus-visible:ring focus-visible:ring-secondary'
 const ghostClassName =
   'bg-transparent hover:underline active:text-secondary-900 transition-colors'
 
@@ -20,11 +23,13 @@ const Button = ({
   onClick,
   className,
   variant = 'primary',
+  type = 'button',
   children,
 }: Props) => {
   return (
     <button
       onClick={onClick}
+      type={type}
       className={clsx(
         defaultClassName,
         {
