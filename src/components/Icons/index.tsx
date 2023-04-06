20a26ad9dd520679a397257e893f95aa237eaa3e
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 12:27:32 -0300

    Reexport Envelope, Lock and Eye icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,11 +1,17 @@
 import Hamburger from './Hamburger'
 import ChevronDown from './ChevronDown'
 import XMark from './XMark'
+import Envelope from './Envelope'
+import Lock from './Lock'
+import Eye from './Eye'
 
 const icons = {
   Hamburger,
   ChevronDown,
   XMark,
+  Envelope,
+  Lock,
+  Eye,
 }
 
 export default icons
