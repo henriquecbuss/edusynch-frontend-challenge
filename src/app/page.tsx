59de9d8eb797db4b9418b2cfe849cebd584dc143
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 17:30:19 -0300

    Implement about us section for mobile and tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,8 @@ const Home = () => {
         height={247}
         className="object-cover object-[40%] h-[180px] -mt-10 sm:h-[247px] sm:object-[50%] md:mt-0"
       />
+
+      <AboutUs />
     </main>
   )
 }
@@ -140,6 +142,87 @@ const Tag = ({ children }: { children: React.ReactNode }) => {
   return (
     <li className="bg-primary-100 text-primary py-1 px-4 rounded-[4px]">
       {children}
+    </li>
+  )
+}
+
+const AboutUs = () => {
+  return (
+    <section className="mt-14 md:mt-20 flex flex-col">
+      <div className="container">
+        <div className="md:ml-[88px]">
+          <h2 className="text-primary font-bold" id="about-us">
+            About us
+          </h2>
+          <p className="text-h4 md:text-h3 font-bold mt-1">Lorem ipsum</p>
+          <p className="text-label mt-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,{' '}
+            purus sit amet luctus venenatis, lectus magna fringilla urna,
+            porttitor
+          </p>
+        </div>
+      </div>
+
+      <ul className="container flex md:flex-col items-center md:items-start overflow-x-scroll pb-14 md:pb-20 gap-4 md:gap-6 mt-6 pt-[11px] md:pt-0 md:mt-10 bg-gradient-to-b from-white to-secondary-100">
+        <div className="flex gap-4 md:gap-6">
+          <AboutUsCard
+            title="For your company"
+            subtitle="Crypto solutions"
+            icon={<Icons.CryptoCurrency />}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          </AboutUsCard>
+
+          <AboutUsCard
+            title="For your company"
+            subtitle="Crypto solutions"
+            icon={<Icons.CryptoCurrencyCircle />}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          </AboutUsCard>
+        </div>
+
+        <div className="flex gap-4 md:gap-6 md:self-end">
+          <AboutUsCard
+            title="For your company"
+            subtitle="Crypto solutions"
+            icon={<Icons.Chart />}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          </AboutUsCard>
+
+          <AboutUsCard
+            title="For your company"
+            subtitle="Crypto solutions"
+            icon={<Icons.Computer />}
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          </AboutUsCard>
+        </div>
+      </ul>
+    </section>
+  )
+}
+
+const AboutUsCard = ({
+  title,
+  subtitle,
+  icon,
+  children,
+}: {
+  title: string
+  subtitle: string
+  icon: React.ReactNode
+  children: React.ReactNode
+}) => {
+  return (
+    <li className="p-6 rounded-md shadow-[0px_12px_24px_rgba(0,0,0,0.05)] flex-shrink-0 w-[200px] md:w-[280px]">
+      <div className="w-10 h-10 md:w-16 md:h-16">{icon}</div>
+      <h3 className="font-bold text-label text-primary mt-4 md:text-base">
+        {title}
+      </h3>
+      <p className="font-bold text-h5 mb-2 md:text-h4">{subtitle}</p>
+      <p className="text-label">{children}</p>
     </li>
   )
 }
