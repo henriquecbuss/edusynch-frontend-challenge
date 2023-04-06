18e1f223a94014bcde204f173cf16fb9fa3d7612
Henrique Buss <henriquebusss@gmail.com>
2023-04-06 09:39:28 -0300

    Fix attributes in hamburger icon

--- src/components/Icons/Hamburger.tsx
+++ src/components/Icons/Hamburger.tsx
@@ -15,15 +15,15 @@ export default function Hamburger({ className }: Props) {
         cy="12"
         r="9"
         stroke="#8C8A97"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
       />
       <path
         stroke="#8C8A97"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
         d="M7.25 12h9.5M7.25 15.25h9.5M7.25 8.75h9.5"
       />
     </svg>
