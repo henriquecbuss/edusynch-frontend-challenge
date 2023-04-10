bd19b60ac91007d6523c77b0b989e0cc694674c3
Henrique Buss <henriquebusss@gmail.com>
2023-04-10 17:43:53 -0300

    Add skeleton dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import SignedInHeader from '@/components/SignedInHeader'
+
+const Dashboard = () => {
+  return (
+    <>
+      <SignedInHeader />
+      <main>This is the dashboard!</main>
+    </>
+  )
+}
+
+export default Dashboard
