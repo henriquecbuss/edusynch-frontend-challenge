31226503317293f6c37325b97342ec81a94a337a
Henrique Buss <henriquebusss@gmail.com>
2023-04-10 18:05:41 -0300

    Add Sign out menu

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,25 +1,51 @@
 'use client'
 
-import { useState } from 'react'
 import clsx from 'clsx'
 import Icons from './Icons'
-import SlideOver from './SlideOver'
+import { Menu, Transition } from '@headlessui/react'
 import Link from 'next/link'
-import SignInButton from './SignInButton'
-import SignUpButton from './SignUpButton'
+import { Fragment } from 'react'
 
 type Props = {
   className?: string
 }
 
 const User = ({ className }: Props) => {
-  // TODO - Implement signed in view
   return (
-    <div className={clsx('flex items-center', className)}>
-      <div className="w-6 h-6 rounded-full bg-secondary-600 mr-2"></div>
-      <p className="hidden md:block text-label mr-1">Aulus</p>
-      <Icons.ChevronDown className="w-2 h-2" />
-    </div>
+    <Menu as="div" className={clsx('relative', className)}>
+      <Menu.Button className="flex items-center">
+        <div className="w-6 h-6 rounded-full bg-secondary-600 mr-2"></div>
+        <p className="hidden md:block text-label mr-1">Aulus</p>
+        <Icons.ChevronDown className="w-2 h-2" />
+      </Menu.Button>
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 mt-2 p-1 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <Menu.Item>
+            {({ active }) => (
+              <Link
+                href="/"
+                className={clsx(
+                  'whitespace-nowrap text-right text-label transition-colors p-1 rounded',
+                  {
+                    'bg-quaternary-200 text-quaternary-700': active,
+                  }
+                )}
+              >
+                Sign out
+              </Link>
+            )}
+          </Menu.Item>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
 
