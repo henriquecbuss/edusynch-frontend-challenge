64c3ede6be34d8cf62b8fbad3ee9e4b26231cac0
Henrique Buss <henriquebusss@gmail.com>
2023-04-12 12:32:19 -0300

    Protect pages

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,28 @@
-import { withClerkMiddleware } from '@clerk/nextjs/server'
+import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+const publicPaths = ['/']
+
+const isPublic = (path: string) => {
+  return publicPaths.find((x) =>
+    path.match(new RegExp(`^${x}$`.replace('*$', '($|/)')))
+  )
+}
+
 export default withClerkMiddleware((req: NextRequest) => {
+  // return NextResponse.next()
+
+  if (isPublic(req.nextUrl.pathname)) {
+    return NextResponse.next()
+  }
+  // if the user is not signed in redirect them to the sign in page.
+  const { userId } = getAuth(req)
+
+  if (!userId) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
   return NextResponse.next()
 })
 
