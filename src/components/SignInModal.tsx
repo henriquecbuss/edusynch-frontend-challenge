f8c9f467d86f79ee1c08f88fc8d2188ba37f3299
Henrique Buss <henriquebusss@gmail.com>
2023-04-12 12:11:50 -0300

    Wire up signInModal with clerk

--- src/components/SignInModal.tsx
+++ src/components/SignInModal.tsx
@@ -8,11 +8,18 @@ import * as Modal from './Modal'
 import useModal from '@/hooks/useModal'
 import { Form, Formik } from 'formik'
 import { useRouter } from 'next/navigation'
+import { useAuth, useSignIn } from '@clerk/nextjs'
 
 const SignInModal = () => {
   const { isOpen, close } = useModal('signIn')
   const { open: openSignUpModal } = useModal('signUp')
   const router = useRouter()
+  const { isLoaded, signIn, setActive } = useSignIn()
+  const { isSignedIn } = useAuth()
+
+  if (!isLoaded || isSignedIn) {
+    return null
+  }
 
   return (
     <Modal.Root isOpen={isOpen} close={close}>
@@ -25,31 +32,71 @@ const SignInModal = () => {
           email: '',
           password: '',
         }}
-        onSubmit={(values, { setSubmitting }) => {
-          close()
-          router.push('/dashboard')
+        onSubmit={async (values, { setSubmitting, setFieldError }) => {
+          try {
+            const signInResult = await signIn.create({
+              identifier: values.email,
+              password: values.password,
+            })
+
+            if (signInResult.status === 'complete') {
+              router.push('/dashboard')
+              close()
+              setActive({ session: signInResult.createdSessionId })
+              return
+            }
+
+            setFieldError('password', 'Something went wrong')
+          } catch (err) {
+            const error = err as {
+              errors: {
+                message: string
+                meta: {
+                  paramName: string
+                }
+              }[]
+            }
+
+            const firstError = error.errors[0]
+            setFieldError(firstError.meta.paramName, firstError.message)
+          } finally {
+            setSubmitting(false)
+          }
         }}
       >
-        <Form className="mt-6">
-          <Input type="email" name="email" placeholder="Email" required />
-          <Input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="mt-6"
-            required
-          />
-          <Link
-            href="#"
-            className="text-small-label text-secondary hover:underline block w-max ml-auto mt-[9px]"
-          >
-            Forgot password?
-          </Link>
+        {({ isSubmitting }) => (
+          <Form className="mt-6">
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              disabled={isSubmitting}
+            />
+            <Input
+              type="password"
+              name="password"
+              placeholder="Password"
+              className="mt-6"
+              required
+              disabled={isSubmitting}
+            />
+            <Link
+              href="#"
+              className="text-small-label text-secondary hover:underline block w-max ml-auto mt-[9px]"
+            >
+              Forgot password?
+            </Link>
 
-          <Button className="mt-4 w-full py-3" type="submit">
-            Sign in
-          </Button>
-        </Form>
+            <Button
+              className="mt-4 w-full py-3"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Sign in
+            </Button>
+          </Form>
+        )}
       </Formik>
 
       <div className="text-small-label mt-4">
